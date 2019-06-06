5d398e0416554f71849abf8ea5a536a96490b464
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-06-05 21:21:03 -0700

    Created the routes for the html. The form posts to the server, tables gets the form info

--- app/server.js
+++ app/server.js
@@ -9,6 +9,9 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var reservations = [];
+var waitingList = [];
+
 // Get and Post requests
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./views/home.html"));
@@ -26,6 +29,25 @@ app.get("/tables.html", function(req, res) {
   res.sendFile(path.join(__dirname, "./views/tables.html"));
 });
 
+app.post("/api/reservations", function(req, res) {
+  var newReservations = req.body;
+
+  if (reservations.length < 5) {
+    reservations.push(newReservations);
+    console.log(reservations);
+  } else {
+    waitingList.push(newReservations);
+    console.log(waitingList);
+  }
+});
+
+app.get("/api/reservations", function(req, res) {
+  console.log(res);
+
+  res.json(reservations);
+});
+
+// =============================End=========================
 app.listen(PORT, function() {
   console.log("App Listening on PORT: " + PORT);
 });
